6e271b081ac35fbd8f8b2bbfde2634a881cdac5b
motoyasaphmusan <tuduluugandalimited@gmail.com>
2026-08-30 00:06:56 +0300

    feat: update main.ts configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -22,6 +22,8 @@ async function bootstrap() {
     origin: [
       process.env.FRONTEND_URL || "http://localhost:3000",
       "http://127.0.0.1:3000",
+      "https://tudulu.org",
+      "https://www.tudulu.org",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
